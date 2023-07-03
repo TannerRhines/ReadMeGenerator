4559b6f7869b0578b328fd80ffeda46ae00d78e2
Tanner Rhines <tannerrhines@protonmail.com>
2023-07-02 21:42:51 -0800

    added generate markdown data

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,17 +12,39 @@ function renderLicenseSection(license) {
   }
 
 };
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# 
+  
+  ${data.title}
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  -[Installation](##Installation)
+  -[Test-Required](##Test-Required)
+  -[Contributors](##Contributors)
+  -[Contact-Info](##Contact-Info)
+
+  ## License Used
+  ${data.license}
+
+  ## Installation
+  ${data.installation}
+
+  ## Data Usage
+  ${data.usage}
+
+  ## Tests Required
+  ${data.test}
+
+ 
+  # Contact Info
+  Have additional questions? Please reach me at ${data.email}
+
+  # Contributors
+  ${data.contributors}
 
 `;
 }
