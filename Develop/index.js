f45aab42594ce0c491f4559ce62ee8943535cf23
Tanner Rhines <tannerrhines@protonmail.com>
2023-07-02 20:24:23 -0800

    added fs require and required markdownJS file

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,11 @@
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
 
+const fs = require('fs');
+
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
